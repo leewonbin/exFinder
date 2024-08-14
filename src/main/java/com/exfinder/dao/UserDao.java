dd7f61a709e401a14f45b2ac1e6936ed736824e8
won bin Lee <lwbkj8546@naver.com>
2024-08-14 10:54:39 +0900

    getUser 메소드 및 매퍼 추가

--- src/main/java/com/exfinder/dao/UserDao.java
+++ src/main/java/com/exfinder/dao/UserDao.java
@@ -6,6 +6,7 @@ import com.exfinder.dto.UserDto;
 
 public interface UserDao {
 	public ArrayList<UserDto> getUserList();
+	public UserDto getUser(String u_id);
 	public int insertUser(UserDto dto);
 	public int updateUser(UserDto dto);
 	public int deleteUser(String u_id);
--- src/main/java/com/exfinder/service/UserServiceImpl.java
+++ src/main/java/com/exfinder/service/UserServiceImpl.java
@@ -2,12 +2,18 @@ package com.exfinder.service;
 
 import java.util.ArrayList;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.exfinder.dao.UserDao;
 import com.exfinder.dto.UserDto;
 
 @Service
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private SqlSession sqlSession;
 
 	@Override
 	public ArrayList<UserDto> getUserList() {
@@ -15,6 +21,12 @@ public class UserServiceImpl implements UserService {
 		return null;
 	}
 
+	@Override
+	public UserDto getUser(String u_id) {
+		UserDao dao = sqlSession.getMapper(UserDao.class);
+		return dao.getUser(u_id);
+	}
+	
 	@Override
 	public int insertUser(UserDto dto) {
 		// TODO Auto-generated method stub
@@ -32,5 +44,6 @@ public class UserServiceImpl implements UserService {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
 
 }
