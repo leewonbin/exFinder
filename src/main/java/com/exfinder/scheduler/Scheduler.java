542926fffa56b4ab08623821c76327890e9474e1
won bin Lee <lwbkj8546@naver.com>
2024-09-13 18:19:07 +0900

    스케줄러 기능 구현중

---
+++ src/main/java/com/exfinder/scheduler/Scheduler.java
@@ -0,0 +1,22 @@
+package com.exfinder.scheduler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.exfinder.service.ExchangeRateService;
+
+@Component
+public class Scheduler {
+	
+//	@Autowired
+//	private ExchangeRateService service;
+	
+//	@Scheduled(cron = "*/10 * * * * ?")
+//	public void schedul() throws Exception {
+//		System.out.println("들어옴");
+//		service.todayExchangeRate();
+//	}
+
+
+}
