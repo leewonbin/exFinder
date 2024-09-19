d1225e9dca3ec7c228e9bcd9fd608a0ee11b71f7
kim jeong ho <zzx5678@naver.com>
2024-09-19 17:35:11 +0900

    뉴스 크롤링 기능 완성

--- src/main/java/com/exfinder/controller/NewsCrawlController.java
+++ src/main/java/com/exfinder/controller/NewsCrawlController.java
@@ -53,8 +53,14 @@ public class NewsCrawlController {
         System.out.println("크롤링할 주소: " + address); // 디버깅용 주소 출력
 
         // 뉴스 제목과 URL을 추출
+        ArrayList<String> newsIcons = new ArrayList<>(); // 뉴스 아이콘 리스트
+        ArrayList<String> newsNames = new ArrayList<>(); // 뉴스 이름 리스트
+        ArrayList<String> newsTimes = new ArrayList<>(); // 뉴스 실시간 리스트
+        
         ArrayList<String> newsURLs = new ArrayList<>(); // 뉴스 URL 리스트
         ArrayList<String> newsTitles = new ArrayList<>(); // 뉴스 제목 리스트
+        ArrayList<String> newsTexts = new ArrayList<>(); // 뉴스 내용 리스트
+        ArrayList<String> newsImgs = new ArrayList<>(); // 뉴스 이미지 리스트
 
         // 네이버 뉴스의 4개 항목 (id: sp_nws1, sp_nws2, sp_nws3, sp_nws4)을 가져오기
         for (int i = 1; i <= 4; i++) {
@@ -64,25 +70,66 @@ public class NewsCrawlController {
             
             // 각 뉴스 항목에서 URL과 제목을 추출하여 리스트에 저장
             for (Element news : newsElements) {
+            	String newsIcon = news.select("a.info.press img.thumb").attr("data-lazysrc"); // 뉴스 아이콘 추출
+            	String newsName = news.select("a.info.press").text(); // 뉴스 이름 추출
+            	String newsTime = news.select("span.info").text(); // 뉴스 실시간 추출
+            	
                 String newsURL = news.select("a.news_tit").attr("href"); // 뉴스 URL 추출
                 String newsTitle = news.select("a.news_tit").attr("title"); // 뉴스 제목 추출
-
+                String newsText = news.select("a.api_txt_lines.dsc_txt_wrap").text(); // 뉴스 내용 추출
+                String newsImg = news.select("a.dsc_thumb img.thumb").attr("data-lazysrc"); // 뉴스 이미지 추출 a.dsc_thumb 
+                
+                /*
+                Elements imgElements = news.select("img");
+                for (Element img : imgElements) {
+                    newsImg = img.attr("data-lazysrc");
+                    if (!newsImg.isEmpty()) {
+                        System.out.println("추출된 이미지 URL: " + newsImg);
+                    }
+                }
+                */
+                
                 // 각 뉴스 URL과 제목을 출력 (디버깅용)
-                System.out.println("뉴스 제목: " + newsTitle);
+                System.out.println("뉴스 아이콘: " + newsIcon);
+                System.out.println("뉴스 이름: " + newsName);
+                System.out.println("뉴스 시간: " + newsTime);
+                
                 System.out.println("뉴스 URL: " + newsURL);
+                System.out.println("뉴스 제목: " + newsTitle);
+                System.out.println("뉴스 내용: " + newsText);  
+                System.out.println("뉴스 img: " + newsImg);
+                
+                newsIcons.add(newsIcon); // URL 리스트에 추가
+                newsNames.add(newsName); // 제목 리스트에 추가
+                newsTimes.add(newsTime); // 이미지 리스트에 추가
                 
                 newsURLs.add(newsURL); // URL 리스트에 추가
                 newsTitles.add(newsTitle); // 제목 리스트에 추가
+                newsTexts.add(newsText); // 내용 리스트에 추가
+                newsImgs.add(newsImg); // 이미지 리스트에 추가
             }
         }
 
         // 크롤링 완료 후 최종 리스트 출력 (디버깅용)
-        System.out.println("최종 뉴스 제목 리스트: " + newsTitles);
+        System.out.println("최종 뉴스 아이콘 리스트: " + newsIcons);
+        System.out.println("최종 뉴스 이름 리스트: " + newsNames);
+        System.out.println("최종 뉴스 시간 리스트: " + newsTimes);
+        
         System.out.println("최종 뉴스 URL 리스트: " + newsURLs);
+        System.out.println("최종 뉴스 제목 리스트: " + newsTitles);
+        System.out.println("최종 뉴스 내용 리스트: " + newsTexts);
+        System.out.println("최종 뉴스 img 리스트: " + newsImgs);
+        
         
         // Model 객체에 뉴스 URL과 제목을 속성으로 추가하여 JSP에 전달
+        model.addAttribute("newsIcons", newsIcons);
+        model.addAttribute("newsNames", newsNames);
+        model.addAttribute("newsTimes", newsTimes);
+        
         model.addAttribute("newsURLs", newsURLs);
         model.addAttribute("newsTitles", newsTitles);
+        model.addAttribute("newsTexts", newsTexts);
+        model.addAttribute("newsImgs", newsImgs);
         return "news"; // news/news.jsp로 이동
     }
 }
---
+++ src/main/webapp/resources/js/exFinder_Currency.js
@@ -0,0 +1,240 @@
+//날짜를 문자열로 포맷팅하는 함수
+function formatDate(date) {
+    var year = date.getFullYear();
+    var month = ('0' + (date.getMonth() + 1)).slice(-2); // 월은 0부터 시작하므로 +1
+    var day = ('0' + date.getDate()).slice(-2); // 일자는 2자리로 맞추기
+    return year + '/' + month + '/' + day;
+}
+
+// 오늘 날짜를 가져옴
+var today = new Date();
+var formattedDate = formatDate(today);
+
+// Google Charts 라이브러리 로드
+google.charts.load('current', {
+    'packages': ['corechart']
+});
+
+$(document).ready(function() {
+    // AJAX 요청
+    ajaxData(currencyCode, 'chart_7day', 'seven-day');
+//    ajaxData(`${currencyDto.c_code}`, 'chart_1month', 'one-month'); 
+//    ajaxData(`${currencyDto.c_code}`, 'chart_3month', 'three-month'); 
+//    ajaxData(`${currencyDto.c_code}`, 'chart_1year', 'one-year'); 
+
+	fetchExchangeRateData(currencyCode, formattedDate, 'value_Currency');
+    // Google Charts 라이브러리가 로드된 후 차트 그리기
+    google.charts.setOnLoadCallback(function() {
+        console.log('Google Charts 라이브러리 로드 완료');
+    });
+});
+
+// AJAX 요청 함수
+function ajaxData(c_code, chartDivId, cart_day) {
+    var end_date = formattedDate; // 오늘 날짜
+
+    $.ajax({
+        type: "POST",
+        url: "/ex/currency/chart",
+        data: {
+            c_code: c_code,
+            start_date: '2024/01/01', // 기본 시작 날짜 설정
+            end_date: end_date,
+            cart_day: cart_day
+        },
+        dataType: "json", // Expect JSON response
+        success: function(response) {
+            console.log('응답 데이터 확인');
+            console.log(response); // 응답 데이터 확인
+            drawCharts(response, chartDivId); // 차트 그리기 함수 호출
+        },
+        error: function(xhr, status, error) {
+            console.error("AJAX 요청 오류:", status, error);
+        }
+    });
+}
+
+// 차트 그리기 함수
+function drawCharts(data, chartDivId) {
+    if (typeof google === 'undefined' || !google.visualization || typeof google.visualization.DataTable !== 'function') {
+        console.error("Google Charts 라이브러리가 로드되지 않았습니다. 재시도합니다.");
+        setTimeout(function() {
+            drawCharts(data, chartDivId);
+        }, 1000); // 1초 후 재시도
+        return;
+    }
+
+    var chartData = new google.visualization.DataTable();
+    chartData.addColumn('date', '날짜'); // 날짜 열
+    chartData.addColumn('number', '값'); // 값 열
+
+    // 응답 데이터를 [날짜, 값] 형식으로 변환
+    var formattedData = data.map(function(item) {
+        var date = new Date(item.rate_date.year, item.rate_date.month - 1, item.rate_date.day); // Date 객체 생성
+        return [date, item.base_r]; // [날짜, 값] 형식으로 배열 반환
+    });
+
+    // 데이터가 유효한지 확인
+    if (formattedData.length === 0) {
+        console.error('차트에 표시할 데이터가 없습니다.');
+        return;
+    }
+
+    chartData.addRows(formattedData); // DataTable에 데이터 추가
+
+	 // X축에 표시할 날짜 계산
+    var minDate = new Date(Math.min.apply(null, formattedData.map(function(row) { return row[0].getTime(); })));
+    var maxDate = new Date(Math.max.apply(null, formattedData.map(function(row) { return row[0].getTime(); })));
+    var dateRange = (maxDate - minDate) / 4; // 4로 나누어 5개의 주요 눈금을 생성
+    var ticks = [];
+
+    // 주요 눈금을 계산하여 추가
+    for (var i = 0; i <= 4; i++) {
+        var tickDate = new Date(minDate.getTime() + i * dateRange);
+        ticks.push(tickDate);
+    }
+
+    var options = {
+    	    hAxis: {
+    	        format: 'yy/MM/dd', // 날짜 형식 지정
+    	        ticks: ticks, // X축에 표시할 날짜 설정
+    	        gridlines: {
+    	            count: 5 // X축의 그리드라인 수 설정 (주요 눈금에 맞춤)
+    	        },
+    	        
+    	    },
+    	    vAxis: {
+    	        logScale: false, // 선형 스케일 사용
+    	        gridlines: {
+    	            count: 5, // Y축의 그리드라인 수 설정
+    	            color: 'lightgray' // Y축 그리드라인 색상
+    	        },
+    	        baselineColor: 'black' // Y축 기준선 색상
+    	    },
+    	    colors: ['#a52714'], // 그래프 선 색상
+    	    legend: {
+    	        position: 'none' // 범례 숨기기
+    	    },
+    	    chartArea: {
+    	        width: '80%', // 차트 영역 너비
+    	        height: '70%' // 차트 영역 높이
+    	    }
+    	};
+
+    var chart = new google.visualization.LineChart(document.getElementById(chartDivId));
+    chart.draw(chartData, options);
+}
+
+//정보 함수
+function fetchExchangeRateData(c_code, rate_date, div_id) {
+    $.ajax({
+        url: '/ex/charts/value',
+        type: 'POST',
+        data: {
+            c_code: c_code,
+            rate_date: rate_date
+        },
+        success: function(response) {
+            // console.log('서버 응답:', response); // 서버 응답 확인
+
+            // 응답 데이터가 JSON 객체로 가정
+            const today_base_r = response.today_base_r;
+            const yesterday_base_r = response.yesterday_base_r;
+            const difference = response.difference;
+            const percent = response.percent;
+
+            // 결과 문자열 생성
+            let result;
+            let cssClass;
+
+            if (today_base_r > yesterday_base_r) {
+                result = today_base_r + ' ▲' + difference.toFixed(2) + ' +' + percent.toFixed(2) + '%';
+                cssClass = 'increased';
+                
+            } else if (today_base_r === yesterday_base_r) {
+                result = today_base_r + ' -' + difference.toFixed(2) + ' ' + percent.toFixed(2) + '%';
+                cssClass = 'unchanged';
+                
+            } else {
+                result = today_base_r + ' ▼' + difference.toFixed(2) + ' ' + percent.toFixed(2) + '%';
+                cssClass = 'decreased';
+                
+            }
+
+            console.log('결과 문자열:', result); // 결과 문자열 확인
+            console.log('CSS 클래스:', cssClass); // CSS 클래스 확인
+
+            // HTML 콘텐츠 업데이트
+            const htmlContent = '<div class="' + cssClass + '">' + result + '</div>';
+            console.log('업데이트할 HTML:', htmlContent); // 업데이트할 HTML 확인               
+
+            // HTML 업데이트
+            $('#' + div_id).html(htmlContent);
+           
+            // 업데이트 후 상태 확인
+            console.log('업데이트된 HTML:', $('#' + div_id).html());
+        },
+        error: function(xhr, status, error) {
+            console.error(c_code,'에 대한 데이터를 가져오는 데 실패했습니다:', error);
+            $('#' + div_id).html('<div>데이터를 가져오는 데 실패했습니다.</div>');
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const charts = document.querySelectorAll('.chart_graph');
+    const sevenDayLink = document.getElementById('seven-day');
+    const oneMonthLink = document.getElementById('one-month');
+    const threeMonthLink = document.getElementById('three-month');
+    const oneYearLink = document.getElementById('one-year');
+
+    // 모든 차트를 숨기고, 모든 링크에서 active 제거
+    function hideAllCharts() {
+        charts.forEach(chart => {
+            chart.style.display = 'none';
+        });
+
+        document.querySelectorAll('.Currency_chart_time_select a').forEach(link => {
+            link.classList.remove('active');
+        });
+    }
+
+    // 특정 차트를 보이고, 링크에 active 클래스 추가
+    function showChart(chartId, link) {
+        hideAllCharts(); // 차트와 active 상태 초기화
+        document.getElementById(chartId).style.display = 'block'; // 선택된 차트 표시
+        link.classList.add('active'); // 선택된 링크에 active 클래스 추가
+    }
+
+    // 스크롤 위치를 유지하는 함수
+    function handleChartClick(event, chartId, link) {
+        event.preventDefault(); // 기본 동작 방지
+        const scrollPosition = window.pageYOffset || document.documentElement.scrollTop; // 현재 스크롤 위치 저장
+        showChart(chartId, link); // 차트 표시
+        window.scrollTo(0, scrollPosition); // 스크롤 위치를 유지
+    }
+
+    // 기본적으로 7일 차트를 보여줌
+    showChart('chart_7day', sevenDayLink);
+
+    // 각 링크에 클릭 이벤트 추가
+    sevenDayLink.addEventListener('click', function(event) {
+        handleChartClick(event, 'chart_7day', sevenDayLink);
+        
+    });
+
+    oneMonthLink.addEventListener('click', function(event) {
+        handleChartClick(event, 'chart_1month', oneMonthLink);
+        ajaxData(currencyCode, 'chart_1month', 'one-month'); 
+    });
+
+    threeMonthLink.addEventListener('click', function(event) {
+        handleChartClick(event, 'chart_3month', threeMonthLink);
+        ajaxData(currencyCode, 'chart_3month', 'three-month'); 
+    });
+
+    oneYearLink.addEventListener('click', function(event) {
+        handleChartClick(event, 'chart_1year', oneYearLink);
+        ajaxData(currencyCode, 'chart_1year', 'one-year'); 
+    });
+});
