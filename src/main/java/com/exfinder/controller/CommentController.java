30374e856c55862a71d491f2952e92fb6f83a14e
woong <coldplay__@naver.com>
2024-09-20 14:51:20 +0900

    댓글

--- src/main/java/com/exfinder/controller/CommentController.java
+++ src/main/java/com/exfinder/controller/CommentController.java
@@ -31,85 +31,6 @@ public class CommentController {
 
 	private static final Logger logger = LoggerFactory.getLogger(CommentController.class);
 
-//    @RequestMapping(value = "/create", method = RequestMethod.POST)
-//    public String createComment(@RequestParam("b_id") int b_id, @RequestParam("comm_content") 
-//    							String comm_content, HttpSession session, RedirectAttributes redirectAttributes) {
-//        try {
-//            UserDto userDto = (UserDto) session.getAttribute("dto");
-//            if (userDto == null) {
-//                redirectAttributes.addFlashAttribute("message", "로그인 후 댓글 작성 가능 합니다.");
-//                return "redirect:/ex/board/read?b_id=" + b_id; 
-//            }
-//
-//            CommentDto dto = new CommentDto();
-//            dto.setB_id(b_id);
-//            dto.setU_id(userDto.getU_id());
-//            dto.setComm_content(comm_content);
-//            cs.commentCreate(dto);
-//
-//        } catch (Exception e) {
-//            logger.error("Error creating comment", e);
-//        }
-//        return "redirect:/ex/board/read?b_id=" + b_id; 
-//    }
-//
-//    @RequestMapping(value = "/delete", method = RequestMethod.POST)
-//    public String deleteComment(@RequestParam("comm_id") int comm_id, @RequestParam("b_id") int b_id, 
-//                                HttpSession session, RedirectAttributes redirectAttributes) {
-//        try {
-//            UserDto userDto = (UserDto) session.getAttribute("dto");
-//            if (userDto == null) {
-//                redirectAttributes.addFlashAttribute("message", "로그인 후 댓글 삭제 가능 합니다.");
-//                return "redirect:/ex/board/read?b_id=" + b_id; 
-//            }
-//
-//            CommentDto existingComment = cs.getCommentById(comm_id);
-//            if (existingComment != null && userDto.getU_id().equals(existingComment.getU_id())) {
-//                cs.commentDelete(comm_id);
-//            }
-//
-//        } catch (Exception e) {
-//            logger.error("Error deleting comment", e);
-//        }
-//        return "redirect:/ex/board/read?b_id=" + b_id; 
-//    }
-//
-//    @RequestMapping(value = "/update", method = RequestMethod.POST)
-//    public String updateComment(@RequestParam("comm_id") int comm_id, @RequestParam("comm_content") String comm_content, 
-//                                @RequestParam("b_id") int b_id, HttpSession session, RedirectAttributes redirectAttributes) {
-//        try {
-//            UserDto userDto = (UserDto) session.getAttribute("dto");
-//            if (userDto == null) {
-//                redirectAttributes.addFlashAttribute("message", "로그인 후 댓글 수정 가능 합니다.");
-//                return "redirect:/ex/board/read?b_id=" + b_id; 
-//            }
-//
-//            CommentDto existingComment = cs.getCommentById(comm_id);
-//            if (existingComment != null && userDto.getU_id().equals(existingComment.getU_id())) {
-//                CommentDto dto = new CommentDto();
-//                dto.setComm_id(comm_id);
-//                dto.setComm_content(comm_content);
-//                dto.setU_id(userDto.getU_id());
-//                cs.commentUpdate(dto);
-//            }
-//
-//        } catch (Exception e) {
-//            logger.error("Error updating comment", e);
-//        }
-//        return "redirect:/ex/board/read?b_id=" + b_id;
-//    }
-//
-//    @RequestMapping(value = "/list", method = RequestMethod.GET)
-//    public String listComments(@RequestParam("b_id") int b_id, HttpSession session, Model model) {
-//        try {
-//            List<CommentDto> comments = cs.commentList(b_id);
-//            model.addAttribute("comments", comments);
-//        } catch (Exception e) {
-//            logger.error("Error listing comments", e);
-//        }
-//        return "board/read"; 
-//    }
-	
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public String createComment(@RequestParam("b_id") int b_id, 
 	                            @RequestParam("comm_content") String comm_content, 
@@ -117,7 +38,6 @@ public class CommentController {
 	    try {
 	        UserDto userDto = (UserDto) session.getAttribute("dto");
 	        if (userDto == null) {
-	            rttr.addFlashAttribute("message", "로그인 후 댓글 작성 가능 합니다.");
 	            return "redirect:/board/read?b_id=" + b_id; 
 	        }
 
@@ -126,43 +46,20 @@ public class CommentController {
 	        dto.setU_id(userDto.getU_id());
 	        dto.setComm_content(comm_content);
 	        cs.commentCreate(dto);
-
-	        // 댓글 등록 후 게시글 읽기 페이지로 리다이렉트
 	        return "redirect:/board/read?b_id=" + b_id; 
+	        
 	    } catch (Exception e) {
 	        logger.error("Error creating comment", e);
 	        return "redirect:/board/read?b_id=" + b_id; // 에러 발생 시에도 게시글 읽기 페이지로
 	    }
 	}
-
-    @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public String deleteComment(@RequestParam("comm_id") int comm_id, @RequestParam("b_id") int b_id, 
-                                HttpSession session, RedirectAttributes rttr) {
-        try {
-            UserDto userDto = (UserDto) session.getAttribute("dto");
-            if (userDto == null) {
-                rttr.addFlashAttribute("message", "로그인 후 댓글 삭제 가능 합니다.");
-                return "redirect:/board/read?b_id=" + b_id; 
-            }
-
-            CommentDto existingComment = cs.getCommentById(comm_id);
-            if (existingComment != null && userDto.getU_id().equals(existingComment.getU_id())) {
-                cs.commentDelete(comm_id);
-            }
-
-        } catch (Exception e) {
-            logger.error("Error deleting comment", e);
-        }
-        return "redirect:/board/read?b_id=" + b_id; 
-    }
-
+	
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public String updateComment(@RequestParam("comm_id") int comm_id, @RequestParam("comm_content") String comm_content, 
                                 @RequestParam("b_id") int b_id, HttpSession session, RedirectAttributes rttr) {
         try {
             UserDto userDto = (UserDto) session.getAttribute("dto");
             if (userDto == null) {
-                rttr.addFlashAttribute("message", "로그인 후 댓글 수정 가능 합니다.");
                 return "redirect:/board/read?b_id=" + b_id; 
             }
 
@@ -181,17 +78,26 @@ public class CommentController {
         return "redirect:/board/read?b_id=" + b_id;
     }
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String listComments(@RequestParam("b_id") int b_id, HttpSession session, Model model) {
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public String deleteComment(@RequestParam("comm_id") int comm_id, @RequestParam("b_id") int b_id, 
+                                HttpSession session, RedirectAttributes rttr) {
         try {
-            List<CommentDto> comments = cs.commentList(b_id);
-            model.addAttribute("comments", comments);
+            UserDto userDto = (UserDto) session.getAttribute("dto");
+            if (userDto == null) {
+                return "redirect:/board/read?b_id=" + b_id; 
+            }
+
+            CommentDto existingComment = cs.getCommentById(comm_id);
+            if (existingComment != null && userDto.getU_id().equals(existingComment.getU_id())) {
+                cs.commentDelete(comm_id);
+            }
+
         } catch (Exception e) {
-            logger.error("Error listing comments", e);
+            logger.error("Error deleting comment", e);
         }
-        return "board/read"; 
+        return "redirect:/board/read?b_id=" + b_id; 
     }
-    
+
     @RequestMapping(value = "/listAll", method = RequestMethod.GET)
     public String listAllComments(@RequestParam("b_id") int b_id, HttpSession session, Model model) {
         logger.info("Fetching all comments for b_id: " + b_id);
@@ -212,7 +118,6 @@ public class CommentController {
         try {
             UserDto userDto = (UserDto) session.getAttribute("dto");
             if (userDto == null) {
-                rttr.addFlashAttribute("message", "로그인 후 대댓글 작성 가능 합니다.");
                 return "redirect:/board/read?b_id=" + parent_id; 
             }
 
--- src/main/webapp/WEB-INF/views/main/exFinder_Currency.jsp
+++ src/main/webapp/WEB-INF/views/main/exFinder_Currency.jsp
@@ -1,15 +1,18 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 
 <html>
 <head>
 <title>통화 상세 정보</title>
-	<!-- jQuery -->
-	<script src="https://code.jquery.com/jquery.min.js"></script>
-	<!-- google charts -->
-	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-	<link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/resources/css/exFinder_Currency.css">
-	<script type="text/javascript" src="${pageContext.request.contextPath}/resources/js/exFinder_Currency.js"></script>
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery.min.js"></script>
+<!-- google charts -->
+<script type="text/javascript"
+	src="https://www.gstatic.com/charts/loader.js"></script>
+<link rel="stylesheet" type="text/css"
+	href="${pageContext.request.contextPath}/resources/css/exFinder_Currency.css">
+<script type="text/javascript"
+	src="${pageContext.request.contextPath}/resources/js/exFinder_Currency.js"></script>
 <style></style>
 <script>
 	var currencyCode = '${currencyDto.c_code}';
@@ -17,111 +20,145 @@
 </head>
 <body>
 	<%@include file="/WEB-INF/views/header/exFinder_header.jsp"%>
-	
+
 	<div style="width: 100%; height: 110px;"></div>
-	
+
 	<!-- 중앙에 위치시키기 위한 div -->
-    <div class="Currency_view">
-    	<div class="Currency_header">
-    		<img src="${pageContext.request.contextPath}/resources/img/gonfalon/${currencyDto.c_code}.png">
-            <a><b>${currencyDto.c_country}</b> ${currencyDto.c_code}</a> 
-            <i class="circle" id="toggle-list">
-        		<i class="arrow-up"></i>
-    		</i>
-    	</div>
-    	<div class="Currency_chart">
-    	    <div class="Currency_left_chart">
-    	    	<div class="Currency_left_chart_header">
-    	    		<a>매매 기준율</a>
-    	    		<div id="value_Currency"></div>
-    	    	</div>
-    	    	<table class="Currency_left_chart_table">    			
-     		   		<tr>
-            			<th colspan="2">현찰</th>
-            			<th colspan="2">송금 (전신환)</th>
-        			</tr>   			
-        			<tr>
-            			<th>사실때</th>
-            			<td>${exchangeRateDto.cash_buy}</td>
-            			<th>보낼때</th>
-            			<td>${exchangeRateDto.ttb}</td>
-        			</tr>
-	        			<tr>
-    	        		<th>파실때</th>
-        	    		<td>${exchangeRateDto.cash_sell}</td>
-            			<th>받을때</th>
-            			<td>${exchangeRateDto.tts}</td>
-        			</tr>
-				</table>
-				<div>
-					기준일 : ${exchangeRateDto.rate_date}
+	<div class="Currency_view">
+		<div class="Currency_header">
+			<img
+				src="${pageContext.request.contextPath}/resources/img/gonfalon/${currencyDto.c_code}.png">
+			<a><b>${currencyDto.c_country}</b> ${currencyDto.c_code}</a> <i
+				class="circle" id="toggle-list"> <i class="arrow-up"></i>
+			</i>
+		</div>
+		<div class="Currency_chart">
+			<div class="Currency_left_chart">
+				<div class="Currency_left_chart_header">
+					<a>매매 기준율</a>
+					<div id="value_Currency"></div>
 				</div>
-    		</div>
-    	    <div class="Currency_right_chart">
-    	    	<div class="Currency_chart_time_select">
-    	    		<ul>
-						<li><a href="#" id="seven-day" >7일</a></li>
-        				<li><a href="#" id="one-month" >1개월</a></li>
-        				<li><a href="#" id="three-month">3개월</a></li>
-        				<li><a href="#" id="one-year">1년</a></li>
+				<table class="Currency_left_chart_table">
+					<tr>
+						<th colspan="2">현찰</th>
+						<th colspan="2">송금 (전신환)</th>
+					</tr>
+					<tr>
+						<th>사실때</th>
+						<td>${exchangeRateDto.cash_buy}</td>
+						<th>보낼때</th>
+						<td>${exchangeRateDto.ttb}</td>
+					</tr>
+					<tr>
+						<th>파실때</th>
+						<td>${exchangeRateDto.cash_sell}</td>
+						<th>받을때</th>
+						<td>${exchangeRateDto.tts}</td>
+					</tr>
+				</table>
+				<div>기준일 : ${exchangeRateDto.rate_date}</div>
+			</div>
+			<div class="Currency_right_chart">
+				<div class="Currency_chart_time_select">
+					<ul>
+						<li><a href="#" id="seven-day">7일</a></li>
+						<li><a href="#" id="one-month">1개월</a></li>
+						<li><a href="#" id="three-month">3개월</a></li>
+						<li><a href="#" id="one-year">1년</a></li>
 					</ul>
-    	    	</div>
-    	    	<div id="chart_7day" class="chart_graph" ></div>
-    	    	<div id="chart_1month" class="chart_graph" ></div>
-    	    	<div id="chart_3month" class="chart_graph" ></div>
-    	    	<div id="chart_1year" class="chart_graph" ></div>
-    		</div>
-    	</div>
-    	<div class="Currency_news-list">
-    	     <!-- 뉴스 리스트 출력 -->
-    		<div class="news-list">
-        		<!-- EL을 사용하여 newsTitles와 newsURLs 출력 -->
-        		<c:forEach var="i" items="${newsTitles}" varStatus="status">
-            		<c:if test="${status.index < 4}">
-                	<div class="news-item">
-                		<div class="new-header">
-                			<img src="${newsIcons.get(status.index)}">${newsNames.get(status.index)} - ${newsTimes.get(status.index)}
-                		</div>
-                		<div class="new-main" onclick="window.location.href='${newsURLs.get(status.index)}';">
-                			<div class="new-main-text">
-                				<label>${newsTitles.get(status.index)} </label><br>
-                				<a>${newsTexts.get(status.index)}</a>
-                			</div>
-                			<img src="${newsImgs.get(status.index)}" alt="뉴스 이미지">
-                		</div>
-                	</div>
-            		</c:if>
-        		</c:forEach>	
-    		</div>
-    	</div>
-    </div>
+				</div>
+				<div id="chart_7day" class="chart_graph"></div>
+				<div id="chart_1month" class="chart_graph"></div>
+				<div id="chart_3month" class="chart_graph"></div>
+				<div id="chart_1year" class="chart_graph"></div>
+			</div>
+		</div>
+		<div class="Currency_news-list">
+			<!-- 뉴스 리스트 출력 -->
+			<div class="news-list">
+				<!-- EL을 사용하여 newsTitles와 newsURLs 출력 -->
+				<c:forEach var="i" items="${newsTitles}" varStatus="status">
+					<c:if test="${status.index < 4}">
+						<div class="news-item">
+							<div class="new-header">
+								<img src="${newsIcons.get(status.index)}">${newsNames.get(status.index)}
+								- ${newsTimes.get(status.index)}
+							</div>
+							<div class="new-main"
+								onclick="window.location.href='${newsURLs.get(status.index)}';">
+								<div class="new-main-text">
+									<label>${newsTitles.get(status.index)} </label><br> <a>${newsTexts.get(status.index)}</a>
+								</div>
+								<img src="${newsImgs.get(status.index)}" alt="뉴스 이미지">
+							</div>
+						</div>
+					</c:if>
+				</c:forEach>
+			</div>
+		</div>
+	</div>
+
+
+	<h1 class="rate-day">일자별</h1>
+
+	<div class="box-contents">
+		<div>
+			<table class="exchange">
+				<thead>
+					<tr class="chart">
+
+						<th rowspan="2">날짜</th>
+						<th rowspan="2">종가</th>
+						<th rowspan="2">매매기준율</th>
+						<th rowspan="2">전일비</th>
+						<th colspan="2">현찰</th>
+						<th colspan="2">송금(전신환)</th>
+						<th rowspan="2">환가로율</th>
+					</tr>
+					<tr class="chartB">
+						<th class="leftL">사실때</th>
+						<th>파실때</th>
+						<th>보내실때</th>
+						<th>받으실때</th>
+					</tr>
+				</thead>
+				<tbody>
+					
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+
+
+
 </body>
 <script>
-/*
-<span id="currency-list" class="hidden">
-<a>미국 USD</a>
-<a>일본 JPY</a>
-<a>유럽연합 EUR</a>
-<a>중국 CNY</a>
-<a>영국 GBP</a>
-		
-<a>스위스 CHF</a>
-<a>캐나다 CAD</a>
-<a>인도 INR</a>
-<a>홍콩 HKD</a>
-<a>이집트 EGP</a>
-		
-<a>사우디 SAR</a>
-<a>호주 AUD</a>
-<a>태국 THB</a>
-<a>러시아 RUB</a>
-<a>베트남 VND</a>
-		
-<a>남아공 ZAR</a>
-<a>멕시코 MXN</a>
-<a>브라질 BRL</a>
-<a>이스라엘 ILS</a>
-<a>뉴질랜드 NZD</a> 		
-</span>*/
+	/*
+	 <span id="currency-list" class="hidden">
+	 <a>미국 USD</a>
+	 <a>일본 JPY</a>
+	 <a>유럽연합 EUR</a>
+	 <a>중국 CNY</a>
+	 <a>영국 GBP</a>
+	
+	 <a>스위스 CHF</a>
+	 <a>캐나다 CAD</a>
+	 <a>인도 INR</a>
+	 <a>홍콩 HKD</a>
+	 <a>이집트 EGP</a>
+	
+	 <a>사우디 SAR</a>
+	 <a>호주 AUD</a>
+	 <a>태국 THB</a>
+	 <a>러시아 RUB</a>
+	 <a>베트남 VND</a>
+	
+	 <a>남아공 ZAR</a>
+	 <a>멕시코 MXN</a>
+	 <a>브라질 BRL</a>
+	 <a>이스라엘 ILS</a>
+	 <a>뉴질랜드 NZD</a> 		
+	 </span>*/
 </script>
 </html>
