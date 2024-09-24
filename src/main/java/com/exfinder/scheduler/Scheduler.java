310fb0c34563282fee73db6799467cd4a89d3a29
won bin Lee <lwbkj8546@naver.com>
2024-09-24 09:36:57 +0900

    스케줄러 테스트 코드 주석

--- src/main/java/com/exfinder/scheduler/Scheduler.java
+++ src/main/java/com/exfinder/scheduler/Scheduler.java
@@ -31,22 +31,21 @@ public class Scheduler {
 	@Autowired
 	private AlramService al_service;
 
-//	@Scheduled(cron = "0 0 9-23 * * ?")
-//	@Scheduled(cron = "0 */2 * * * ?")
-//	public void hourScheduled() throws Exception {
-//		try {
-//			String[] curr = e_service.currSelect();
-//			ArrayList<NoticeExchangeRateDto> list = n_service.check(curr);
-//			for(NoticeExchangeRateDto dto : list) {
-//				n_service.insert(dto);
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace(); // 예외 출력
-//		}
-//		System.out.println("addAlram실행");
+	@Scheduled(cron = "0 0 9-23 * * ?")
+	public void hourScheduled() throws Exception {
+		try {
+			String[] curr = e_service.currSelect();
+			ArrayList<NoticeExchangeRateDto> list = n_service.check(curr);
+			for(NoticeExchangeRateDto dto : list) {
+				n_service.insert(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace(); // 예외 출력
+		}
+		System.out.println("addAlram실행");
 //		addAlram();
-//		
-//	}
+		
+	}
 	
 	@Scheduled(cron = "0 0 0 * * ?")
 	public void midnightScheduled() throws Exception {
@@ -56,21 +55,21 @@ public class Scheduler {
 			e_service.exchangeRateInsert(dto);
 		}
 	}
-	@Scheduled(cron = "0 */2 * * * ?")
-	public void addAlram() {
-		System.out.println("addAlram 들어옴");
-		try {
-			ArrayList<NotificationDto> list = no_service.exchangeEqulasCheck();
-			System.out.println("list.size() : " + list.size());
-			if(list.size() != 0) {
-				for(NotificationDto dto : list) {
-					al_service.alramInsert(dto);
-				}
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
+//	@Scheduled(cron = "0 */2 * * * ?")
+//	public void addAlram() {
+//		System.out.println("addAlram 들어옴");
+//		try {
+//			ArrayList<NotificationDto> list = no_service.exchangeEqulasCheck();
+//			System.out.println("list.size() : " + list.size());
+//			if(list.size() != 0) {
+//				for(NotificationDto dto : list) {
+//					al_service.alramInsert(dto);
+//				}
+//			}
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//	}
 }
